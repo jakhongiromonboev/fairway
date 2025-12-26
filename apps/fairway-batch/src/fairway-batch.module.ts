c2035b5881cf3eb2117a9fe335df98756fdeb33b
jakhongiromonboev <jakhongiromonboev@gmail.com>
2025-12-26 22:27:41 +0900

    feat: configurre env variables and install ALL necessary packages

--- apps/fairway-batch/src/fairway-batch.module.ts
+++ apps/fairway-batch/src/fairway-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FairwayBatchController } from './fairway-batch.controller';
 import { FairwayBatchService } from './fairway-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [FairwayBatchController],
   providers: [FairwayBatchService],
 })
--- apps/fairway-batch/src/main.ts
+++ apps/fairway-batch/src/main.ts
@@ -3,6 +3,6 @@ import { FairwayBatchModule } from './fairway-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(FairwayBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
