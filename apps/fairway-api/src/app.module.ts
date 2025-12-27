ee007e0d843391376b2887dc67d5ff5bdc68a029
jakhongiromonboev <jakhongiromonboev@gmail.com>
2025-12-27 19:10:03 +0900

    feat: inegration of graphQl and Global error handling & prettier

--- apps/fairway-api/src/app.module.ts
+++ apps/fairway-api/src/app.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { T } from './libs/types/common';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+			formatError: (error: T) => {
+				const graphQLFormattedError = {
+					code: error?.extensions.code,
+					message:
+						error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+				};
+				console.log('GRAPHQL GLOBAL ERROR:', graphQLFormattedError);
+				return graphQLFormattedError;
+			},
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
